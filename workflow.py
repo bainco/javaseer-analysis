c357fba3de74ecd3374754c480e567cf4d1eb64a
Connor Bain <bainco@email.sc.edu>
2016-04-19 12:37:04 -0500

    Added workflow.py

---
+++ workflow.py
@@ -0,0 +1,7 @@
+execfile("IDtoCondition.py")
+# This loads in the IDtoCondition CSV file into two dictionaries
+# 1. The IDtoConditionDictionary
+# 2. the IDtoRunsDictionary
+
+execfile("errors_reader.py")
+# This process the DB dump.csv into a bunch of dictionaries
