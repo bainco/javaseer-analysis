030fab0b3873b475d9d6a4c1d53570137427f61b
Connor Bain <bainco@email.sc.edu>
2016-04-19 22:54:47 -0500

    Added stderr capture.

--- call_javac.py
+++ call_javac.py
@@ -1,22 +1,23 @@
 import subprocess
+from subprocess import call
 
 # I can grab this from the program column!
-loadProgram = """public class HelloPrinter
+loadProgram = """
+public class HelloPrinter
 {
 
 public static void main(String[] args)
 
 {
 
-System.out.println("Hello");//displays text
-
-System.out.print("World");//displays text
-
-//System.out.print("!");
-
+double[] myList = {1.9, 2.9, 3.4, 3.5};
+    for (int i = 0; i < 5; i++) {
+        System.out.println(myList[i]);
+    }
 }
 
-}"""
+}
+"""
 
 # I can just grab the name from the Assignment column
 program_name = """HelloPrinter.java"""
@@ -28,10 +29,10 @@ text_file.write(loadProgram)
 text_file.close()
 
 call(["javac", program_name])
-
-process = subprocess.Popen(["java", program_compiled_name], stdout=subprocess.PIPE)
-out, err = process.communicate()
-print(out)
+proc = subprocess.Popen(["java", program_compiled_name],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout_value, stderr_value = proc.communicate('through stdin to stdout')
+#print '\tpass through:', repr(stdout_value)
+print 'stderr:', repr(stderr_value)
 
 call(["rm", program_name])
 call(["rm", program_compiled_name + ".class"])
